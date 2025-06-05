46938a34d60cb5173d3b531b9e3dc969c43d083f
Jasper Steenwinkel <Jasperevertsteenwinkel@gmail.com>
2025-06-04 21:25:06 -0500

    added the backend

---
+++ task-manager-fullstack/backend/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+# Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{'id': t.id, 'title': t.title, 'completed': t.completed} for t in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.json
+    new_task = Task(title=data['title'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task added'})
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    data = request.json
+    task = Task.query.get(id)
+    task.title = data.get('title', task.title)
+    task.completed = data.get('completed', task.completed)
+    db.session.commit()
+    return jsonify({'message': 'Task updated'})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
